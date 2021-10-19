089c61b643dbbb7b47950149be7c43dcdd8e487b
Mateus Freitas <mateus_freitascorreia@hotmail.com>
2021-10-19 15:03:20 -0300

    organizando_pastas

---
+++ include/veiculo.h
@@ -0,0 +1,16 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
+
+class veiculo
+{
+    public:
+        veiculo();
+        virtual ~veiculo();
+
+    protected:
+
+    private:
+};
+
+#endif // VEICULO_H
