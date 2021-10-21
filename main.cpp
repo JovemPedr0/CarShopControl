974a76c1ae6f939beb36bed3a08e849b222dbc08
Pedro Lucas <81992401+JovemPedr0@users.noreply.github.com>
2021-10-21 01:43:12 -0300

    Finishing

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void CadastroVeiculo(){
     int tipo;
     Veiculo *vcl;
 
-    cout << "Qual tipo de veÃ­culo deseja cadastrar? (1 para MOTO e 2 para CARRO): ";
+    cout << "Qual tipo de veículo deseja cadastrar? (1 para MOTO e 2 para CARRO): ";
     cin >> tipo;
 
     switch(tipo){
@@ -22,7 +22,7 @@ void CadastroVeiculo(){
             break;
 
         default:
-            cout << "Valor InvÃ¡lido" << endl;
+            cout << "Valor Inválido" << endl;
     }
 
     vcl->lerAtributos();
@@ -33,12 +33,21 @@ void CadastroVeiculo(){
     veiculos.push_back(vcl);
 }
 
+void exibeMenu(){
+    cout << "------------------------------------------------------------------------------------------------" << endl;
+        cout << "   1 - Cadastrar Veí­culo" << 
+              "\n   2 - Visualizar Estoque de Veí­culos" << 
+              "\n   3 - Adicionar Veículo ao Estoque" << 
+              "\n   4 - Remover Veí­culo do Estoque" << 
+              "\n   5 - Sair" << endl;
+}
+
 void AdicionarEstoque(){
     int codigo, quantidade, escolha;
 
     while(1){
         
-        cout << "Digite o cÃ³digo do veÃ­culo a adicionar no estoque: " << endl;
+        cout << "Digite o código do veí­culo a adicionar no estoque: " << endl;
         cin >> codigo;
 
         for(unsigned int i = 0; i < veiculos.size(); i++){
@@ -48,7 +57,7 @@ void AdicionarEstoque(){
                 cout << "Digite a quantidade a ser adicionada:" << endl;
                 cin >> quantidade;
 
-                cout << "Escolha em qual loja serÃ¡ adicionada a quantidade de " << quantidade << " veÃ­culos: " << veiculos[i]->getModeloDoVeiculo()
+                cout << "Escolha em qual loja será adicionada a quantidade de " << quantidade << " veículos: " << veiculos[i]->getModeloDoVeiculo()
                 << "(LOJA MATRIZ = 1 / FILIAL = 2)" << endl;
                 cin >> escolha;
 
@@ -61,12 +70,12 @@ void AdicionarEstoque(){
                         lojas[1].adicionarEstoque(quantidade, codigo);
                         return;
 
-                        default:
-                        cout << "Loja invÃ¡lida" << endl;
+                    default:
+                        cout << "Loja inválida" << endl;
                     }
 
             }else{
-                cout << "CÃ³digo InvÃ¡lido" << endl;
+                cout << "Código Inválido" << endl;
             }
         }
     }
@@ -77,14 +86,14 @@ void DiminuirEstoque(){
 
     while(1){
         
-        cout << "Digite o cÃ³digo do veÃ­culo a retirar no estoque: " << endl;
+        cout << "Digite o código do veículo a retirar no estoque: " << endl;
         cin >> codigo;
 
         for(unsigned int i = 0; i < veiculos.size(); i++){
 
             if(codigo == veiculos[i]->getCodigoDoVeiculo()){
 
-                cout << "Escolha em qual loja serÃ¡ vendido o veÃ­culo: " << veiculos[i]->getModeloDoVeiculo()
+                cout << "Escolha em qual loja será vendido o veí­culo: " << veiculos[i]->getModeloDoVeiculo()
                 << "(LOJA MATRIZ = 1 / FILIAL = 2)" << endl;
                 cin >> escolha;
 
@@ -97,12 +106,12 @@ void DiminuirEstoque(){
                         lojas[1].diminuirEstoque(codigo);
                         return;
 
-                        default:
-                        cout << "Loja invÃ¡lida" << endl;
+                    default:
+                        cout << "Loja inválida" << endl;
                     }
 
             }else{
-                cout << "CÃ³digo InvÃ¡lido" << endl;
+                cout << "Código Inválido" << endl;
             }
         }
     }
@@ -113,37 +122,30 @@ int main(){
 
     lojas[0] = Concessionaria("Loja Matriz", Endereco("10102300", "Avenida Nego", 99));
     lojas[1] = Concessionaria("Loja Filial", Endereco("59871234", "Avenida Tirandentes", 45));
-
+   
     while (opcao != 5){
-
-        cout << "1 - Cadastrar VeÃ­culo" << "\n2 - Visualizar Estoque de VeÃ­culos" << "\n3 - Adicionar VeÃ­culo ao Estoque" << "\n4 - Remover VeÃ­culo do Estoque" << "\n5 - Sair" << endl;
+        system("cls");
+        exibeMenu();
         cin >> opcao;
 
         switch(opcao){
             case 1:
                 system("cls");
                 CadastroVeiculo();
-                
                 break;
 
             case 2:
                 system("cls");
                 cout << "Digite qual estoque deseja ver: ('1 - para a loja Matriz e 2 - para a Filial')" << endl;
-
-                while(estoqueEscolhido != 1 && estoqueEscolhido != 2){
-                    cin >> estoqueEscolhido;
-
-                    if(estoqueEscolhido != 0 && estoqueEscolhido != 1){
-                        cout << "Estoque nÃ£o existe ou nÃ£o consta no sistema!" << endl;
-                    }      
-                }
-
+                cin >> estoqueEscolhido;
+         
                 if(estoqueEscolhido == 0){
                     lojas[0].exibirEstoque();
+                    system("pause");
                 }else{
                     lojas[1].exibirEstoque();
-                }
-                    
+                    system("pause");
+                }   
                 break;
 
             case 3:
@@ -155,17 +157,22 @@ int main(){
                 system("cls");
                 DiminuirEstoque();
                 break;
-
+            
             case 5:
                 system("cls");
-                cout << "Obrigado por usar nosso sistema!";
-                    for(int i = 0; i < 3; i++){
-                        cout << ".";
-                    }
+                printf("Você pediu para sair, fechando programa\n");
+                printf("Obrigado por usar nosso sistema!");
+                for(int a = 0; a < 3; a++){
+                    printf("=");
+                    printf("D");
+                    printf(" ");
+                    Sleep(300);
+                }
+                exit(0);
                 break;
         }
 
     }
-
+    
     return 0;
 }
